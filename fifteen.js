a293c862bdca80102cdbb92a7a275db7956373fc
Christopher Robinson <chrispaul_robinson@hotmail.com>
2018-10-29 23:51:22 -0500

    added background, and adjusted puzzle

--- fifteen.js
+++ fifteen.js
@@ -1,1 +1,24 @@
 // Author: Christopher Robinson
+
+window.onload = function(){
+	var pArea = document.querySelector("#puzzlearea").querySelectorAll("div");
+	console.log(pArea);
+	
+		var appFrame = () =>{
+		
+		for(var i = 0; i < pArea.length; i++){
+			pArea[i].classList.add("puzzlepiece");
+		}
+	}
+
+	appFrame();
+	
+}
+
+
+
+var appFrame = () =>{
+	for(var i = 0; i < pArea.length; i++){
+		pArea[i].classList.add("puzzlepiece");
+	}
+}
